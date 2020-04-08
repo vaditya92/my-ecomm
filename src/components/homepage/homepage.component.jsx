e76a15eac4273006d3a3ad21a599c4e420ea9ffe
Aditya Varma <vaditya92@gmail.com>
2020-04-08 10:01:12 +0530

    Broken homepage into smaller components

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,52 +1,13 @@
 import React from 'react';
 
+import Directory from './../directory/directory.component';
+
 import './homepage.styles.scss';
 
 
 const Homepage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">
-            <h1>HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">
-            <h1>HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">
-            <h1>HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">
-            <h1>HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">
-            <h1>HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
