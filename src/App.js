d195cecd764913774ddaae1f15703ecda7fbcaac
Aditya Varma <vaditya92@gmail.com>
2020-04-08 18:29:41 +0530

    Added sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 import Header from './components/header/header.component';
+import Auth from './components/auth/auth.component';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={Auth} />
       </Switch>
     </div>
   );
