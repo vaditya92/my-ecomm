9c1b94492b2d73a9dd65fec5fe0606937da5036c
Aditya Varma <vaditya92@gmail.com>
2020-04-08 12:53:11 +0530

    Create shop page displaying list of items

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Homepage from './components/homepage/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop-page/shop-page.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
