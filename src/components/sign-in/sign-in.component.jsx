5bf9ec2e89e463692f2c02e395516f6c81990ca6
Aditya Varma <adityavarma@Adityas-MacBook-Air.local>
2020-04-11 11:55:41 +0530

    Hooked sign in functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from './../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
 
 
-import { signInWithGoogle } from './../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from './../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -18,9 +18,15 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   handleChange = (event) => {
